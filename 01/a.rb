96b2b74e3cd448698cb775731683572264634a11
Manassarn Manoonchai <manassarn@gmail.com>
2022-12-02 00:37:12 +0700

    Add some cats

--- 01/a.rb
+++ 01/a.rb
@@ -1,1 +1,1 @@
-p File.open('input.txt').slice_when { _2 == "\n" }.map { _1.sum(&:to_i) }.max
+p `cat input.txt`.lines.slice_when { _2 == "\n" }.map { _1.sum(&:to_i) }.max
--- 01/b.rb
+++ 01/b.rb
@@ -1,1 +1,1 @@
-p File.open('input.txt').slice_when { _2 == "\n" }.map { _1.sum(&:to_i) }.sort.last(3).sum
+p `cat input.txt`.lines.slice_when { _2 == "\n" }.map { _1.sum(&:to_i) }.sort.last(3).sum
