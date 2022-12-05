c8e0507e11c825ff9d285cc9a9ae8007fc46f918
Manassarn Manoonchai <manassarn@gmail.com>
2022-12-06 01:29:00 +0700

    Strip right

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from sys import stdin, argv
 from importlib import util as importlib_util
 
 file_path = argv[1]
-lines = [line.strip() for line in stdin]
+lines = [line.rstrip() for line in stdin]
 
 spec = importlib_util.spec_from_file_location('solver', file_path)
 module = importlib_util.module_from_spec(spec)
